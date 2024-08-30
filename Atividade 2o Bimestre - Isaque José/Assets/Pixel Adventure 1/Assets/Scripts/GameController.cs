8a68439d698bef595abb98f65f92cdccb3567f13
link.if <145566234+misterlinky@users.noreply.github.com>
2024-08-30 11:35:34 -0300

    apenas corrigindo

--- Atividade 2o Bimestre - Isaque José/Assets/Pixel Adventure 1/Assets/Scripts/GameController.cs
+++ Atividade 2o Bimestre - Isaque José/Assets/Pixel Adventure 1/Assets/Scripts/GameController.cs
@@ -8,26 +8,17 @@ public class GameController : MonoBehaviour
 {
     public int totalScore;
     public TextMeshProUGUI scoreText;
-    public GameObject gameOver;
-    public GameObject nextLevel;
     public GameObject player;
     public static GameController instance;
-    // Start is called before the first frame update
+    
     void Start()
     {
         instance = this;
     }
-public void UpdateScoreText(){
-    scoreText.text = totalScore.ToString();
-}
-public void ShowGameOver(){
-gameOver.SetActive(true);
-}
-public void NextLevelPainel(){
-nextLevel.SetActive(true);
-player.SetActive(false);
-}
-   public void RestartGame(string lvlName){
-    SceneManager.LoadScene(lvlName);
-}
+    public void UpdateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+    }
+
+
 }
